a3b438983bcc65d76da57ce547c61b4591ae5a46
Sharad Kumar Dutta <sharad.k.dutta@gmail.com>
2023-11-03 17:34:47 +0530

    aws-cdk-learning

---
+++ cdk-s3-demo/infra/lib/infra-stack.ts
@@ -0,0 +1,25 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'InfraQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    // s3 bucket
+    const bucket = new s3.Bucket(this, 'Sample-Infra-Bucket', {
+      versioned: true,
+      bucketName: 'sample-infra-bucket',
+      publicReadAccess: false,
+    });
+  }
+}
