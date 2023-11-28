07f43347af2e73d3fca27eb57c6d147daa3d1ad9
Sharad Kumar Dutta <sharad.k.dutta@gmail.com>
2023-11-28 15:04:36 +0530

    Update cdk_vpcendpoint_stack.py

--- cdk-vpcendpoint/cdk_vpcendpoint/cdk_vpcendpoint_stack.py
+++ cdk-vpcendpoint/cdk_vpcendpoint/cdk_vpcendpoint_stack.py
@@ -7,7 +7,7 @@ from constructs import Construct
 
 
 # Replace 'YOUR_VPC_ID' with the ID of your existing VPC
-existing_vpc_id = 'vpc-00e2da4df3a6ea96c'
+existing_vpc_id = 'vpc-0c52e2c30be598770'
 
 class CdkVpcendpointStack(cdk.Stack):
 
@@ -16,20 +16,19 @@ class CdkVpcendpointStack(cdk.Stack):
         # Import existing VPC
         existing_vpc = ec2.Vpc.from_lookup(self, 'ExistingVpc', vpc_id=existing_vpc_id)
 
-        # Define the subnet selection criteria
-       
-        # Filter private subnets from existing VPC
-        # Select private subnets based on the specified criteria
-        subnet_selection = ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
-        # Wrap the subnet selection criteria in a sequence
-        subnets = [subnet_selection]
-        # Create VPC endpoint
-        vpc_endpoint = ec2.GatewayVpcEndpoint(self, 'k0x-demo-vpce', service=ec2.GatewayVpcEndpointAwsService.S3, subnets=subnets, vpc=existing_vpc, )
-
+        # Create security group
         # Create security group for VPC endpoint
         security_group = ec2.SecurityGroup(self, 'k0x-demo-vpce-sg', vpc=existing_vpc, allow_all_outbound=True)
         security_group.add_ingress_rule(ec2.Peer.ipv4(existing_vpc.vpc_cidr_block), ec2.Port.tcp(443))
         security_group.add_ingress_rule(ec2.Peer.ipv4(existing_vpc.vpc_cidr_block), ec2.Port.tcp(7999))
+        
+        # Create VPC endpoint
+        vpc_endpoint = ec2.InterfaceVpcEndpoint(self, 'k0x-demo-vpce', 
+                                              service=ec2.InterfaceVpcEndpointAwsService.S3, 
+                                              subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED), vpc=existing_vpc, 
+                                              private_dns_enabled=False, security_groups=[security_group])
+
+        
 
         # Create hosted zones
         abc_hosted_zone = route53.HostedZone(self, 'AbcHostedZone', zone_name='abc.com')
