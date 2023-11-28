f7c88db55be13a3e7c22d019d0032ce2fa702878
Sharad Kumar Dutta <sharad.k.dutta@gmail.com>
2023-11-28 15:08:09 +0530

    Update app.py

--- cdk-vpcendpoint/app.py
+++ cdk-vpcendpoint/app.py
@@ -4,12 +4,25 @@ import os
 import aws_cdk as cdk
 
 from cdk_vpcendpoint.cdk_vpcendpoint_stack import CdkVpcendpointStack
+
+
 app = cdk.App()
 CdkVpcendpointStack(app, "CdkVpcendpointStack",
-                    env=cdk.Environment( 
-                        account='642252392942', 
-                        region='us-east-1')
-                    )
+    # If you don't specify 'env', this stack will be environment-agnostic.
+    # Account/Region-dependent features and context lookups will not work,
+    # but a single synthesized template can be deployed anywhere.
 
+    # Uncomment the next line to specialize this stack for the AWS Account
+    # and Region that are implied by the current CLI configuration.
+
+    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+
+    # Uncomment the next line if you know exactly what Account and Region you
+    # want to deploy the stack to. */
+
+    env=cdk.Environment(account='642252392942', region='us-east-1'),
+
+    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+    )
 
 app.synth()
